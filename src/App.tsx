23de44318fa9e9bdf6c01ee5ba2efe65dd53e35e
mamenex <leo.rehnstrom@gmail.com>
2026-01-30 12:07:59 +0100

    Add InstallPWA placement to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import InstallPWA from "./components/InstallPWA";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      {/* Install button placed top-right */}
+      <div style={{ position: "fixed", top: 12, right: 12, zIndex: 1000 }}>
+        <InstallPWA />
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
